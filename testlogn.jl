ad316b1987dc41528f68460d4dbef0aa1f04d0ec
Dawei1114 <165422197+Dawei1114@users.noreply.github.com>
2026-04-24 20:51:15 +0000

    Simplify lognormal test in testlogn.jl

--- testlogn.jl
+++ testlogn.jl
@@ -1,9 +1,6 @@
-using Distributions
+using Distributions, Statistics
 
-mean = 1
 var = 0.1
-Y_test = rand(LogNormal(0, var), 1_000_000)
-emp_mean = mean(Y_test)
-emp_std_error = std(Y_test)/sqrt(1_000_000)
-emp_var = var(Y_test)
-prinln("Hello World!")
+mu = -var/2
+Y_test = rand(LogNormal(mu, var), 1_000_000)
+mean(Y_test)
